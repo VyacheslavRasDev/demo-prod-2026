532c55bdfaedfec32a40c38f6d24db9cacc230e0
Vyacheslav Raspertov <vyacheslav.rasDev@yahoo.com>
2026-01-30 15:01:59 +0200

    modified_router

--- routers/web.php
+++ routers/web.php
@@ -2,10 +2,10 @@
 
 $router->get('/', 'controllers/index.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
 
-$router->get('/claims', 'controllers/claims/index.php');
+$router->get('/claims', 'controllers/claims/index.php')->only('auth');
 $router->get('/claim', 'controllers/claims/claim.php');
 $router->get('/claims/create', 'controllers/claims/create.php');
 $router->get('/claim/edit', 'controllers/claims/edit.php');
